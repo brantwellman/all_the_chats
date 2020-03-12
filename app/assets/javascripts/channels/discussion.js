8ad53e3915a9ea077a8534cc5b3738af45fd1d5c
Brant Wellman <BrantWellman@gmail.com>
2020-03-12 10:54:06 -0600

    Add basics of discussion js file?

---
+++ app/assets/javascripts/channels/discussion.js
@@ -0,0 +1,27 @@
+App.discussion = App.cable.subscriptions.create("DiscussionChannel", {
+  connected: function() {
+  // Called when the subscription is ready for use on the server
+},
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+  received: function(data) {
+    console.log("DATA", data)
+  },
+  chat: function(data) {
+    this.perform('chat', data);
+  }
+});
+
+(function() {
+  $(document).on('click', '.submit-button', function() {
+    const input = $('.message-input');
+    const text = input.val();
+    const userId = input.data().userId;
+    
+    App.discussion.chat({
+      text: text,
+      user_id: userId
+    });
+  });
+}).call(this);
